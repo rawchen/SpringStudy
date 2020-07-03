a7d59df8f155500c86b4dfc8e68c7e78a3dcc6ad
YOYLING <2221999792@qq.com>
2020-07-03 23:51:07 +0800

    Add files via upload

---
+++ day_01_04bean/src/main/java/com/yoyling/factory/staticFactory.java
@@ -0,0 +1,13 @@
+package com.yoyling.factory;
+
+import com.yoyling.service.IAccountService;
+import com.yoyling.service.impl.AccountServiceImpl;
+
+public class staticFactory {
+    /**
+     * 模拟一个工厂类（该类可能是存在于jar包中的，我们无法通过修改源码的方式来提供默认构造函数）
+     */
+    public static IAccountService getAccountService(){
+        return new AccountServiceImpl();
+    }
+}
