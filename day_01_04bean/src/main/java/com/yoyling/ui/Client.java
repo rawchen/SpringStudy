aca44d89c9fb51b42b966403b2a280d4e062bb90
YOYLING <2221999792@qq.com>
2020-07-04 23:13:46 +0800

    Add files via upload

--- day_01_04bean/src/main/java/com/yoyling/ui/Client.java
+++ day_01_04bean/src/main/java/com/yoyling/ui/Client.java
@@ -15,13 +15,22 @@ public class Client {
      */
     public static void main(String[] args) {
         //1.获取核心容器对象
-        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml");
         //2.根据id获取Bean对象
-        IAccountService as = (IAccountService)ac.getBean("accountService");
 
-        System.out.println(as);
+        //默认创建单例对象
+        IAccountService as1 = (IAccountService)ac.getBean("accountService");
+        IAccountService as2 = (IAccountService)ac.getBean("accountService");
 
-        as.saveAccount();
+//        System.out.println(as);
+        as1.saveAccount();
+//        System.out.println(as1);
+//        System.out.println(as2);
+//        System.out.println(as1 == as2);
+
+        //手动关闭容器
+        ac.close();
 
 
 
