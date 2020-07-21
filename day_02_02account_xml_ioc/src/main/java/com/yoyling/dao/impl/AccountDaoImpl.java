51a969e1a61c329652339022ce9baaa5c54efbce
YOYLING <2221999792@qq.com>
2020-07-21 22:40:48 +0800

    Add files via upload

---
+++ day_02_02account_xml_ioc/src/main/java/com/yoyling/dao/impl/AccountDaoImpl.java
@@ -0,0 +1,44 @@
+package com.yoyling.dao.impl;
+
+import com.yoyling.dao.AccountDao;
+import com.yoyling.domain.Account;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+
+import java.util.List;
+
+/**
+ * 账户的持久层实现类
+ */
+public class AccountDaoImpl implements AccountDao {
+
+    private QueryRunner queryRunner;
+
+    public void setQueryRunner(QueryRunner queryRunner) {
+        this.queryRunner = queryRunner;
+    }
+
+    public List<Account> findAllAccount() {
+        try {
+            return queryRunner.query("select * from account",new BeanListHandler<Account>(Account.class));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Account findAccountById(Integer accountId) {
+        return null;
+    }
+
+    public void saveAccount(Account account) {
+
+    }
+
+    public void updateAccount(Account account) {
+
+    }
+
+    public void deleteAccount(Integer accountId) {
+
+    }
+}
---
+++ day_02_02account_xml_ioc/src/main/java/com/yoyling/service/impl/AccountServiceImpl.java
@@ -0,0 +1,39 @@
+package com.yoyling.service.impl;
+
+import com.yoyling.dao.AccountDao;
+import com.yoyling.domain.Account;
+import com.yoyling.service.AccountService;
+
+import java.util.List;
+
+/**
+ * 账户的业务实现类
+ */
+public class AccountServiceImpl implements AccountService {
+
+    private AccountDao dao;
+
+    public void setDao(AccountDao dao) {
+        this.dao = dao;
+    }
+
+    public List<Account> findAllAccount() {
+        return dao.findAllAccount();
+    }
+
+    public Account findAccountById(Integer accountId) {
+        return dao.findAccountById(accountId);
+    }
+
+    public void saveAccount(Account account) {
+        dao.saveAccount(account);
+    }
+
+    public void updateAccount(Account account) {
+        dao.updateAccount(account);
+    }
+
+    public void deleteAccount(Integer accountId) {
+        dao.deleteAccount(accountId);
+    }
+}
