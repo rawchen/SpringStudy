8ae81058f08dfe85f5b7950a97c624624adabd96
YOYLING <2221999792@qq.com>
2020-07-29 23:12:27 +0800

    Add files via upload

---
+++ day_02_04account_annoioc_withoutxml/src/main/java/com/yoyling/config/JdbcConfig.java
@@ -0,0 +1,61 @@
+package com.yoyling.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbutils.QueryRunner;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
+/**
+ * 和spring连接数据库相关的配置类
+ */
+//@Configuration
+public class JdbcConfig {
+
+    @Value("${jdbc.driver}")
+    private String driver;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
+
+    /**
+     * 用于创建一个QueryRunner对象
+     * @param dataSource
+     * @return
+     */
+    @Scope("prototype")
+    @Bean(name = "queryRunner")
+    public QueryRunner createQueryRunner(DataSource dataSource) {
+        return new QueryRunner(dataSource);
+    }
+
+    /**
+     * 创建数据源对象
+     * @return
+     */
+    @Bean(name = "dataSource")
+    public DataSource createDataSource() {
+        try {
+            ComboPooledDataSource ds = new ComboPooledDataSource();
+            ds.setDriverClass(driver);
+            ds.setJdbcUrl(url);
+            ds.setUser(username);
+            ds.setPassword(password);
+            return ds;
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
--- day_02_04account_annoioc_withoutxml/src/main/java/com/yoyling/config/SpringConfiguration.java
+++ day_02_04account_annoioc_withoutxml/src/main/java/com/yoyling/config/SpringConfiguration.java
@@ -1,20 +1,15 @@
 package com.yoyling.config;
 
-import com.mchange.v2.c3p0.ComboPooledDataSource;
-import org.apache.commons.dbutils.QueryRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 该类为一个配置类，它的作用和bean.xml是一样的
  * spring中的新注解
  * Configuaration
  *     作用：指定当前类为一个配置类
+ *     细节：当配置类作为AnnotationConfigApplicationContext对象创建的参数时，该注解可不写。
  * ComponentScan
  *     作用：用于通过注解指定spring在创建容器时要扫描的包
  *     属性：
@@ -28,39 +23,23 @@ import java.beans.PropertyVetoException;
  *     细节：
  *         当我们使用注解配置方法时，如果方法有参数，spring框架会去容器中查找有没有可用的bean对象。
  *         查找方式和Autowired注解的作用是一样的
+ * Import
+ *     作用：用于导入其它配置类
+ *     属性：
+ *         value：用于指定其他配置类的字节码
+ *         当我们使用Import注解后，有Import注解的类就是父配置类，而导入的都是子配置类
+ * PropertySource
+ *     作用：用于指定properties文件的位置
+ *     属性：
+ *         value：指定文件的名称和路径
+ *              关键字：classpath：表示类路径下
+ *
  */
-@Configuration
+//@Configuration
 @ComponentScan("com.yoyling")
+@Import(JdbcConfig.class)
+@PropertySource("classpath:jdbcConfig.properties")
 public class SpringConfiguration {
 
-    /**
-     * 用于创建一个QueryRunner对象
-     * @param dataSource
-     * @return
-     */
-    @Scope("prototype")
-    @Bean(name = "queryRunner")
-    public QueryRunner createQueryRunner(DataSource dataSource) {
-        return new QueryRunner(dataSource);
-    }
 
-    /**
-     * 创建数据源对象
-     * @return
-     */
-    @Bean(name = "dataSource")
-    public DataSource createDataSource() {
-        try {
-            ComboPooledDataSource ds = new ComboPooledDataSource();
-            ds.setDriverClass("com.mysql.jdbc.Driver");
-            ds.setJdbcUrl("jdbc:mysql://localhost:3306/springtest");
-            ds.setUser("root");
-            ds.setPassword("root");
-            return ds;
-        } catch (PropertyVetoException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-    }
 }
