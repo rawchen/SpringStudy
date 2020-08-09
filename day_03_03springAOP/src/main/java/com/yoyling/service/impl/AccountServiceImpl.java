ee8ac61633b2e49c795f182faee92c341e689ce8
YOYLING <2221999792@qq.com>
2020-08-09 23:57:19 +0800

    Add files via upload

---
+++ day_03_03springAOP/src/main/java/com/yoyling/service/impl/AccountServiceImpl.java
@@ -0,0 +1,24 @@
+package com.yoyling.service.impl;
+
+import com.yoyling.service.AccountService;
+
+/**
+ * 账户的业务层实现类
+ */
+public class AccountServiceImpl implements AccountService {
+    @Override
+    public void saveAccount() {
+        System.out.println("执行了保存");
+    }
+
+    @Override
+    public void updateAccount(int i) {
+        System.out.println("执行了更新" + i);
+    }
+
+    @Override
+    public int deleteAccount() {
+        System.out.println("执行了删除");
+        return 0;
+    }
+}
