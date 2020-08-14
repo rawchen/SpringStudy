1c499d1d10a3b8aa11d7aff6abe92312d775510f
YOYLING <2221999792@qq.com>
2020-08-14 23:38:34 +0800

    Add files via upload

---
+++ day_04_01jdbcTemplate/src/main/java/com/yoyling/jdbctemplate/JdbcTemplateDemo1.java
@@ -0,0 +1,25 @@
+package com.yoyling.jdbctemplate;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+/**
+ * JdbcTemplate的最基本用法
+ */
+public class JdbcTemplateDemo1 {
+    public static void main(String[] args) {
+        //准备数据源：spring的内置数据源
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/springtest");
+        ds.setUsername("root");
+        ds.setPassword("root");
+
+        //1.创建对象JdbcTemplate
+        JdbcTemplate jt = new JdbcTemplate();
+        //给jt设置数据源
+        jt.setDataSource(ds);
+        //2.执行操作
+        jt.execute("insert into account(name,money)values('ccc',1000)");
+    }
+}
