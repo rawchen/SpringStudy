bb4bbb688f0d63162f556b338ad944a6442c7852
YOYLING <2221999792@qq.com>
2020-08-18 23:58:24 +0800

    Add files via upload

---
+++ day_04_05transactionXml/src/main/java/com/yoyling/service/AccountService.java
@@ -0,0 +1,24 @@
+package com.yoyling.service;
+
+import com.yoyling.domain.Account;
+
+/**
+ * 账户的业务层接口
+ */
+public interface AccountService {
+
+    /**
+     * 根据id查询账户信息
+     * @param id
+     * @return
+     */
+    Account findAccountById(Integer id);
+
+    /**
+     * 转账
+     * @param sourceName    转出账户名称
+     * @param targetName    转入账户名称
+     * @param money         转账金额
+     */
+    void transfer(String sourceName, String targetName, Float money);
+}
